0519dec856810b735b326cd76373d5d790775c84
Dario Balboni <dario.balboni.96@gmail.com>
2024-02-13 11:54:42 +0100

    Add support for code blocks highlight and copy button

--- ui/src/ChatBot.wc.svelte
+++ ui/src/ChatBot.wc.svelte
@@ -1,13 +1,26 @@
-<svelte:options customElement="hpc-chatbot" />
+<svelte:options customElement={{ tag: "hpc-chatbot" }} />
 
 <script>
- import { onMount } from "svelte"
+ import { onMount, tick } from "svelte"
+ import { get_current_component } from "svelte/internal"
  import Showdown from "showdown"
+ import Highlight from "highlight.js"
+ import CopyButtonPlugin from "highlightjs-copy"
  import { Ollama } from "./lib/ollama.js"
  import { Chat } from "./lib/chats.js"
  import config from "./lib/config.js"
  import timeAgo from "./lib/timeAgo.js"
 
+ import highlightCopyStyle from "highlightjs-copy/dist/highlightjs-copy.min.css?raw"
+ import highlightStyle from "highlight.js/styles/atom-one-dark.css?raw"
+
+ const self = get_current_component()
+ // inject full CSS styles for highlight
+ const style = document.createElement("style")
+ style.textContent += highlightStyle
+ style.textContent += highlightCopyStyle
+ self.$$.root.appendChild(style)
+ 
  const mdConverter = new Showdown.Converter()
  
  let ollama // ollama object to interact with the server
@@ -15,7 +28,13 @@
  let model // model that has been chosen as the best currently available
  let chat // chat that is currently visible on the screen
 
+ Highlight.configure({
+     languageDetectRe: /\blanguage-([\w-]+)\b/i,
+ })
+ Highlight.addPlugin(new CopyButtonPlugin())
+
  onMount(async () => {
+     // load ollama model and understand its status
      const baseUrl = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`
      ollama = new Ollama(baseUrl)
      status = ollama.status
@@ -31,10 +50,21 @@
  let chatBotOpen = false
  let chatBotExpanded = false
 
+ async function highlightCode() {
+     const chatbotWCs = document.querySelectorAll("hpc-chatbot")
+     for (let chatbotwc of chatbotWCs)
+	 for (let codeblock of chatbotwc.shadowRoot.querySelectorAll("pre code:not([data-highlighted])"))
+	     Highlight.highlightElement(codeblock)
+ }
+ 
  async function handleUserMessage() {
+     // TODO: Check if CTRL key is pressed and ignore
      chat.addMessage({ role: "user", content: text })
      text = ""
+     await highlightCode()
      await chat.complete(model)
+     await tick()
+     await highlightCode()
  }
 
  let msgTextSize = "text-base" 
@@ -51,16 +81,15 @@
     <span title={status ? $status.description : ""}>{status ? $status.icon : ""}</span>
 </button>
 
-<div class="absolute bottom-0 right-0 top-0 left-0 px-2 py-2 w-full h-full flex flex-row justify-end pointer-events-none" class:hidden={!chatBotOpen}>
+<div id="chatbot" class="absolute bottom-0 right-0 top-0 left-0 px-2 py-2 w-full h-full flex flex-row justify-end pointer-events-none" class:hidden={!chatBotOpen}>
     <content class="bg-violet-100 text-base rounded-md h-full px-4 py-2 z-50 pointer-events-auto
 		    flex flex-col items-stretch justify-between"
 	     class:w-full={chatBotExpanded} class:max-w-1-3={!chatBotExpanded}>
-	<div class="mb-4 font-bold text-xl flex flex-row items-stretch justify-between">
-	    <div>
-		<span>✨ HPC Assistant</span>
-		<span title={status ? $status.description : ""}>{status ? $status.icon : ""}</span>
+	<div class="mb-4 font-bold text-xl flex flex-row flex-wrap-reverse items-stretch justify-between">
+	    <div class="flex flex-row flex-no-wrap justify-start flex-auto py-1">
+		<div title={status ? $status.description : ""}>✨ HPC Assistant {status ? $status.icon : ""}</div>
 	    </div>
-	    <div>
+	    <div class="flex flex-row flex-no-wrap justify-end flex-auto py-1">
 		<button title="Change the messages text size" on:click={cycleTextSize}>🗛</button>
 		{#if !chatBotExpanded}
 		    <button title="Expand full width" on:click={() => chatBotExpanded = true}>↖️</button>
@@ -78,7 +107,7 @@
 			     class:bg-sky-200={message.role == "user"} class:place-self-end={message.role == "user"}
 			     class:bg-lime-200={message.role == "assistant"} class:place-self-start={message.role == "assistant"}>
 			    
-			    <div class="mb-2 flex flex-row justify-between items-baseline">
+			    <div class="mb-2 flex flex-row flex-wrap justify-between items-baseline">
 				<span class="font-bold">{#if message.role == "user"}You{:else}Assistant{/if}</span>
 				<span class="text-grey-500 text-sm">{$timeAgo(message.timestamp)}</span>
 			    </div>
@@ -113,3 +142,4 @@
      max-width: 33.33%;
  }
 </style>
+
